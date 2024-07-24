6087cbdc156919664ada975b3f11a06b9ce94473
00Prakash00 <prakashd_ai21@velhightech.com>
2024-07-24 10:57:03 +0530

    Add files via upload

---
+++ webbank/src/main/java/com/webbank/servlet/Transaction.java
@@ -0,0 +1,53 @@
+package com.webbank.servlet;
+
+import java.sql.Timestamp;
+
+public class Transaction {
+    private int transactionId;
+    private String accountNo;
+    private Timestamp transactionDate;
+    private String transactionType;
+    private double currentBalance;
+
+    // Getters and Setters
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Timestamp getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Timestamp transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public double getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(double currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+	public void setAccountNo(String accountNo) {
+		// TODO Auto-generated method stub
+		this.accountNo = accountNo;
+		
+	}
+	public String getAccountNo() {
+		return accountNo;
+    }
+}
